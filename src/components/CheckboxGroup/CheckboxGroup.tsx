83cf187d23abdbd8c8a5d0bfe423071f6b5215f6
Thanh Tùng <ht@thanhtunguet.info>
2019-12-21 17:29:02 +0700

    Format

--- src/components/CheckboxGroup/CheckboxGroup.tsx
+++ src/components/CheckboxGroup/CheckboxGroup.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './CheckboxGroup.scss';
 
-
 function CheckboxGroup() {
   return (
     <>
--- src/components/RadioGroup/RadioGroup.tsx
+++ src/components/RadioGroup/RadioGroup.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './RadioGroup.scss';
 
-
 function RadioGroup() {
   return (
     <>
