5d7e797dfb2806a1fefe913a25c29b364f951fb2
thanhtunguet <thanhtung.uet@gmail.com>
2020-02-17 14:32:45 +0700

    Refactor

--- src/index.tsx
+++ src/index.tsx
@@ -22,28 +22,17 @@ Promise.all([
 ])
   .then(() => {
     const root: HTMLDivElement = document.getElementById('root') as HTMLDivElement;
-    if (root.hasChildNodes()) {
-      ReactDOM.hydrate(
-        <BrowserRouter>
-          <AppLoading>
-            <App routes={routes}/>
-          </AppLoading>
-        </BrowserRouter>,
-        root,
-      );
-    } else {
-      ReactDOM.render(
-        <BrowserRouter>
-          <AppLoading>
-            <App routes={routes}/>
-          </AppLoading>
-        </BrowserRouter>,
-        root,
-      );
-    }
-  });
+    (root.hasChildNodes() ? ReactDOM.hydrate : ReactDOM.render)(
+      <BrowserRouter>
+        <AppLoading>
+          <App routes={routes}/>
+        </AppLoading>
+      </BrowserRouter>,
+      root,
+    );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    serviceWorker.unregister();
+  });
