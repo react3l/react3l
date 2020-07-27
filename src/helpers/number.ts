9797bec3e610e9fd23ed3fc1ea65c32104633115
tungpt46 <tungpt46@fpt.com.vn>
2020-07-27 07:05:00 +0700

    Update test-rc29

--- src/helpers/number.ts
+++ src/helpers/number.ts
@@ -5,6 +5,11 @@ export function numberWithCommas(x: number): string {
   return '';
 }
 
+export function isValidNumber(v: any): boolean {
+  return typeof v === 'number' && !Number.isNaN(v);
+}
+
 export default {
   numberWithCommas,
+  isValidNumber,
 };
