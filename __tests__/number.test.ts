88dbdfc4657e621bd6263aecf2c7880ea52b88c9
Thanh Tùng <ht@thanhtunguet.info>
2020-11-10 20:00:54 +0700

    Remove tests

--- __tests__/number.test.ts
+++
@@ -1,8 +0,0 @@
-import { isValidNumber, numberWithCommas } from '@react3l/react3l/helpers';
-
-test('Number helper', () => {
-  expect(isValidNumber(NaN)).toBeFalsy();
-  expect(isValidNumber(123)).toBeTruthy();
-
-  expect(numberWithCommas(100000)).toEqual('100,000');
-});
--- __tests__/path.test.ts
+++
@@ -1,5 +0,0 @@
-import {url} from '@react3l/react3l/helpers/path';
-
-test('url works', () => {
-  expect(url('http://localhost', 'api', 'sample')).toEqual('http://localhost/api/sample');
-});
